67379f07145c3b9c1d3cb40fc682baf5dd47f193
bahib <bah49983@gmail.com>
2025-01-12 23:01:52 +0000

    a third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> 
+    {/* <Link to={"/apropos"}></Link> */}
+      <Link to={"/apropos"}>Retour dans à propos</Link>
+      <Link to={"/blog"}>Visitez le blog</Link>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, Link, Links, RouterProvider } from 'react-router-dom'
+import Article from './Article.jsx'
+import PageErreur from './PageErreur.jsx'
 
+const route = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <PageErreur />
+    },
+    {
+      path: "apropos",
+      element: <>
+      <h1>A propos</h1>
+      <p>Ceci est un fragment React.</p>
+      <Link to={"/"}>Retour à la page principale</Link>
+      </>
+    },
+    {
+      path: "/blog",
+      element: (
+        <>
+      <h1>Liste des articles</h1>
+      <p>Article 11.</p>
+      <Link to={"/blog/200"}>Article 11.</Link>
+      {/* <Link to={"/"}>Retour à la page principale</Link> */}
+      </>
+      )
+    },
+    {
+      path: "/blog/:id",
+      element: <Article />
+    }
+  ]
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router= {route} />
   </StrictMode>,
 )
